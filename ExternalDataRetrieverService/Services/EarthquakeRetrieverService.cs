950aec63faa3b4702a44ca89d0de88c01c310895
Liam Treacy <liam.treacy@opencastsoftware.com>
2022-12-14 10:11:34 +0000

    Getting the data from the US feed

--- ExternalDataRetrieverService/Services/EarthquakeRetrieverService.cs
+++ ExternalDataRetrieverService/Services/EarthquakeRetrieverService.cs
@@ -1,9 +1,25 @@
+using System.Net;
+
 namespace ExternalDataRetrieverService.Services;
 
 public class EarthquakeRetrieverService
 {
+    private static string GeoFeed =
+        "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary" +
+        "/" +
+        "all_hour.geojson";
+    
     public string Get()
     {
-        return "Hello... from get";
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(GeoFeed);
+        request.AutomaticDecompression = 
+            DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+        using(HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+        using(Stream stream = response.GetResponseStream())
+        using(StreamReader reader = new StreamReader(stream))
+        {
+            return reader.ReadToEnd();
+        }
     }
 }
